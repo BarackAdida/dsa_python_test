df4cc76a32690cff63c2c73b136677df793de69f
BarackAdida <adida.barack@student.moringaschool.com>
2026-04-20 12:12:44 +0300

    New branch and loops

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
 print("I am sick!")
+sentence = "Bow your wretched heads befor the Lord of Greed...!"
+for i in range(2, 20):
+    print(sentence)
+    
+name = "Barack"
+age = 25
+balance = 27859.67
+flag = True
+
+print(type(name))
+print(type(age))
+print(type(balance))
+print(type(flag))
+print(id(sentence))
+
+"""
+multiline comments
+multiline comments
+multiline comments
+"""
+
+items = [1, 2, 3, 4, 5, 6]
+for item in items:
+    print(item)
+    
+for i in range(18):
+    if i == 17:
+        print(i)
