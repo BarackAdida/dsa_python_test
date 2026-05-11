a21d575352776b29c2917cadae54f8baf3ee0322
BarackAdida <adida.barack@student.moringaschool.com>
2026-05-11 11:14:48 +0300

    Implemented recursion

--- recursion.py
+++ recursion.py
@@ -17,9 +17,9 @@ def recursion(n):
 recursion(2)
 
 def a(n):
-    # if n > 0:
-    #     print(f"Inside a {n}")
-    #     return b(n - 1)
+    if n > 0:
+        print(f"Inside a {n}")
+        return b(n - 1)
     
     if n < 1:
         return n
@@ -33,3 +33,23 @@ def b(n):
         return a(n - 1)
     
 a(5)
+
+# tail recursion
+def rec(n):
+    if n < 1:
+        return n
+    
+    print(n)
+    return rec(n - 1)
+
+rec(2)
+
+#head recursion
+def head(n):
+    if n < 1:
+        return n
+    
+    head(n - 1)
+    print(n)
+    
+head(5)
